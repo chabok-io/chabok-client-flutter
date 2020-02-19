ff22cee79117a83fbab5ec761b07a9f896e39df2
farbod <farbod.ps@gmail.com>
2020-02-19 12:56:17 +0330

    add application class in sample project

--- example/android/app/src/main/java/com/adpdigital/chabok/starter/MainActivity.java
+++ example/android/app/src/main/java/com/adpdigital/chabok/starter/MainActivity.java
@@ -2,10 +2,6 @@ package com.adpdigital.chabok.starter;
 
 import androidx.annotation.NonNull;
 
-import com.adpdigital.push.AdpPushClient;
-import com.adpdigital.push.LogLevel;
-import com.adpdigital.push.config.Environment;
-
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -13,9 +9,6 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 public class MainActivity extends FlutterActivity {
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        AdpPushClient.setLogLevel(LogLevel.VERBOSE);
-        AdpPushClient.configureEnvironment(Environment.SANDBOX);
-
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 }
