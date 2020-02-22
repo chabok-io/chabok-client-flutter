ffcd1982d8055a9a1c88497ce025dcc189cc87ea
farbod <farbod.ps@gmail.com>
2020-02-22 14:30:07 +0330

    add notification service for sample app

---
+++ example/ios/NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by Farbod on 12/3/1398 AP.
+//  Copyright © 1398 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
